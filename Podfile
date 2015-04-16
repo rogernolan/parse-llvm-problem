1b6af67c01f3109c86af4d2510b62dbfb7084cd5
Rog <rog@hatbat.net>
2015-04-16 10:00:27 +0100

    first commit

---
+++ Podfile
@@ -0,0 +1,11 @@
+# Uncomment this line to define a global platform for your project
+# platform :ios, '6.0'
+
+target 'testproject' do
+	pod 'Parse', '~> 1.7.1'
+end
+
+target 'testproject' do
+
+end
+
---
+++ testproject/model.swift
@@ -0,0 +1,15 @@
+//
+//  model.swift
+//  testproject
+//
+//  Created by roger nolan on 16/04/2015.
+//  Copyright (c) 2015 Schibsted. All rights reserved.
+//
+
+import Foundation
+import Parse
+
+class Model : Parse {
+    
+}
+
